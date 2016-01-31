609695df383741f48765edb7f64a147489789b6c
Song Zhou <zhousong1993@gmail.com>
2016-01-31 17:40:41 +0800

    add `UIrefreshControl`

--- 9Day/9Day/CustomTableViewController.swift
+++ 9Day/9Day/CustomTableViewController.swift
@@ -14,17 +14,6 @@ class CustomTableViewController: UITableViewController {
     private let moreArray: [String] = ["fish", "carrotos", "bread", "chicken", "water"]
     
     @IBAction func topRefresh(sender: UIRefreshControl) {
-        
-        /**
-        关于闭包内使用strong  weak unowned 可以参考下面的官方文档
-        https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/AutomaticReferenceCounting.html
-        */
-        /** 
-        如何声明一个weak对象
-        这个和oc中__weak 同理...
-        weak public var delegate: protocol<UIImagePickerControllerDelegate, UINavigationControllerDelegate>?
-        weak var weakSelf = self
-        */
         dispatch_after(2, dispatch_get_main_queue()) { () -> Void in
             self.dataArray += self.moreArray
             sender.endRefreshing()
